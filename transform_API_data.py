4177f3bfca42055280f5a7d53ac55762f0032562
Daniel Contreras <daniel.contreras_d@uao.edu.co>
2025-04-06 22:01:27 -0500

    Transformation task (API)

--- transform_API_data.py
+++ transform_API_data.py
@@ -6,18 +6,24 @@ TEMP_PATH = "/home/dcontreras/Workshop002/temp"
 
 def transform_api_data():
     try:
-        logging.info("Reading API-enriched Spotify data")
+        logging.info("Reading API Spotify data")
         df = pd.read_csv(f"{TEMP_PATH}/spotify_API.csv")
 
-        df.drop_duplicates(inplace=True)
-
-        df.dropna(subset=["artist", "musicbrainz_id"], inplace=True)
+        if "artists" in df.columns:
+            df.rename(columns={"artists": "artist"}, inplace=True)
 
         df.columns = df.columns.str.lower().str.replace(' ', '_')
 
-        output_path = os.path.join(TEMP_PATH, "api_transformed.csv")
+        df.dropna(subset=["artist", "musicbrainz_id"], inplace=True)
+
+        # Eliminar columna duration_ms si existe
+        if "duration_ms" in df.columns:
+            df.drop(columns=["duration_ms"], inplace=True)
+            logging.info("Removed 'duration_ms' column from API dataset")
+
+        output_path = os.path.join(TEMP_PATH, "spotify_api_transformed.csv")
         df.to_csv(output_path, index=False)
-        logging.info(f"API data transformed and saved to {output_path}")
+        logging.info(f"API-enriched data transformed and saved to {output_path}")
         return True
 
     except Exception as e:
