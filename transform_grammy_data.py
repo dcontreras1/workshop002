f2523866e0e52470914282e41fa82c6c5b59d727
Daniel Contreras <daniel.contreras_d@uao.edu.co>
2025-04-06 22:00:12 -0500

    Transformation grammy task

--- transform_grammy_data.py
+++ transform_grammy_data.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine, text
 from airflow.hooks.base import BaseHook
+import pandas as pd
 import logging
+import os
+
+TEMP_PATH = "/home/dcontreras/Workshop002/temp"
 
 def transform_grammy_data():
     logging.info("Transforming Grammy Data")
@@ -13,29 +17,45 @@ def transform_grammy_data():
         with engine.connect() as conn:
             logging.info("Connected to the database")
 
+            # Eliminar duplicados
             conn.execute(text("""
-                DELETE FROM grammy_awards
-                WHERE id IN (
-                    SELECT id FROM (
-                        SELECT id, ROW_NUMBER() OVER (PARTITION BY artist, song, category, year ORDER BY id) AS row_num
-                        FROM grammy_awards
-                    ) t WHERE row_num > 1
+                DELETE FROM grammy_awards_table
+                WHERE ctid NOT IN (
+                    SELECT MIN(ctid)
+                    FROM grammy_awards_table
+                    GROUP BY artist, nominee, category, year
                 )
             """))
 
+            # Reemplazar valores nulos
             conn.execute(text("""
-            UPDATE grammy_awards
-            SET artist = COALESCE(artist, 'Unknown'),
-                song = COALESCE(song, 'Unknown')
+                UPDATE grammy_awards_table
+                SET artist = COALESCE(artist, 'Unknown'),
+                    nominee = COALESCE(nominee, 'Unknown')
             """))
 
+            # Añadir columna binaria para "winner"
             conn.execute(text("""
-                ALTER TABLE grammy_awards ADD COLUMN IF NOT EXISTS winner_binary INT;
-                UPDATE grammy_awards
+                ALTER TABLE grammy_awards_table ADD COLUMN IF NOT EXISTS winner_binary INT;
+                UPDATE grammy_awards_table
                 SET winner_binary = CASE WHEN winner = 'Yes' THEN 1 ELSE 0 END;
             """))
 
-            logging.info("Data from grammy_awards transformed successfully")
+        # Cargar los datos a un DataFrame para transformar columnas específicas
+        df = pd.read_sql("SELECT * FROM grammy_awards_table", engine)
+
+        # Formatear las fechas
+        for col in ["published_at", "updated_at"]:
+            if col in df.columns:
+                df[col] = pd.to_datetime(df[col]).dt.date
+
+        # Eliminar la columna year si existe
+        if "year" in df.columns:
+            df.drop(columns=["year"], inplace=True)
+
+        output_path = os.path.join(TEMP_PATH, "grammy_transformed.csv")
+        df.to_csv(output_path, index=False)
+        logging.info(f"Transformed Grammy data saved to {output_path}")
 
     except Exception as e:
         logging.error(f"Error transforming data: {e}")
